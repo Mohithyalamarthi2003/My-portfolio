ad4385a46919bb1e024d7df58b72499f574907cb
Mohithyalamarthi2003 <yalamarthymohithchowdhary@gmail.com>
2025-12-07 19:08:20 +0530

    Fix asset paths after moving assets into src

--- src/components/utiles/Cdn.js
+++ src/components/utiles/Cdn.js
@@ -1,19 +1,19 @@
 // src/components/utils/Cdn.js
 
-import Logo from '../../assets/logo.avif';
-import image from '../../assets/coding.webp';
-import java from '../../assets/java.webp';
-import duo1 from '../../assets/IMG-20250928-WA0006.jpg';
-import duo2 from '../../assets/IMG-20250928-WA0008.jpg';
-import duo3 from '../../assets/IMG-20250928-WA0011.jpg';
-import duo4 from '../../assets/IMG-20250928-WA0012.jpg';
-import duo5 from '../../assets/IMG-20250928-WA0013.jpg';
-import Java from '../../assets/Java.jpg';
-import css from '../../assets/css1.webp';
-import js from '../../assets/javascript.jpg';
-import react from '../../assets/react.jpg';
-import tailcss from '../../assets/tailcss.png';
-import manual from '../../assets/manual.jpg';
+import Logo from '../../assests/logo.avif';
+import image from '../../assests/coding.webp';
+import java from '../../assests/java.webp';
+import duo1 from '../../assests/IMG-20250928-WA0006.jpg';
+import duo2 from '../../assests/IMG-20250928-WA0008.jpg';
+import duo3 from '../../assests/IMG-20250928-WA0011.jpg';
+import duo4 from '../../assests/IMG-20250928-WA0012.jpg';
+import duo5 from '../../assests/IMG-20250928-WA0013.jpg';
+import Java from '../../assests/Java.jpg';
+import css from '../../assests/css1.webp';
+import js from '../../assests/javascript.jpg';
+import react from '../../assests/react.jpg';
+import tailcss from '../../assests/tailcss.png';
+import manual from '../../assests/manual.jpg';
 
 export {
   Logo,
